632ef3ac44153337516b7b2a482b806c97843e3a
villiger9 <ali.shaker93@gmail.com>
2026-03-17 13:39:44 +0300

    fixed cv link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ export default function Home() {
                 </NavLink>
 
                 <a
-                  href="/Ali Shaker__CV.pdf"
+                  href="/Portfolio/Ali%20Shaker__CV.pdf"
                   target="_blank"
                   className="btn btn-primary shadow-sm"
                 >
